2e1143caebf0298a9e4e5294f34ef00e310d1f2c
grey123son <94587086+grey123son@users.noreply.github.com>
2022-12-18 11:52:25 -0800

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,47 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDCIr3F2Hc7PkpYE0oWJGzTXRneiStgEZw",
+    authDomain: "chatapp-5b710.firebaseapp.com",
+    databaseURL: "https://chatapp-5b710-default-rtdb.firebaseio.com",
+    projectId: "chatapp-5b710",
+    storageBucket: "chatapp-5b710.appspot.com",
+    messagingSenderId: "671167886598",
+    appId: "1:671167886598:web:54337b004d1f29c86ada87"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+function logout() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+  firebase_message_id = childKey;
+  message_data = childData;
+//Start code
+console.log(firebase_message_id);
+console.log(message_data);
+name = message_data['name'];
+message = message_data['message'];
+like = message_data['like'];
+name_with_tag = "<h4> "+ name +" <img class='user_tick src=tick.png></h4>";
+message_with_tag = "<h4 class='message_h4>"+ message +"</h4>";
+like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
+
+row = name_with_tag + message_with_tag + like_button + span_with_tag;
+document.getElementById("output").innerHTML += row;
+//End code
+} });  }); }
+getData();
+function send(){
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+  });
+
+  document.getElementById("msg").value;
+}
